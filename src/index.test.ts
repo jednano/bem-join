e7c9ada5c3b5664990390671270cfdf68f48c1d2
Jed Mao <jedmao@outlook.com>
2018-02-03 21:17:09 -0600

    v1.0.0 - Initial release

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,70 +1,70 @@
 import test from 'ava'
 
-import bemBlock from './'
+import bemJoin from './'
 
-test('calling bemBlock("foo")() returns "foo"', t => {
-	t.is(bemBlock('foo')(), 'foo')
+test('calling bemJoin("foo")() returns "foo"', t => {
+	t.is(bemJoin('foo')(), 'foo')
 })
 
-test('calling bemBlock("foo")({ bar: true }) returns "foo foo--bar"', t => {
+test('calling bemJoin("foo")({ bar: true }) returns "foo foo--bar"', t => {
 	t.is(
-		bemBlock('foo')({ bar: true }),
+		bemJoin('foo')({ bar: true }),
 		'foo foo--bar',
 	)
 })
 
-test('calling bemBlock("foo")({ bar: false }) returns "foo"', t => {
+test('calling bemJoin("foo")({ bar: false }) returns "foo"', t => {
 	t.is(
-		bemBlock('foo')({ bar: false }),
+		bemJoin('foo')({ bar: false }),
 		'foo',
 	)
 })
 
-test('calling bemBlock("foo")({ bar: true, baz: false }) returns "foo foo--bar"', t => {
+test('calling bemJoin("foo")({ bar: true, baz: false }) returns "foo foo--bar"', t => {
 	t.is(
-		bemBlock('foo')({ bar: true, baz: false }),
+		bemJoin('foo')({ bar: true, baz: false }),
 		'foo foo--bar',
 	)
 })
 
-test('calling bemBlock("foo")("bar") returns "foo__bar"', t => {
+test('calling bemJoin("foo")("bar") returns "foo__bar"', t => {
 	t.is(
-		bemBlock('foo')('bar'),
+		bemJoin('foo')('bar'),
 		'foo__bar',
 	)
 })
 
-test('calling bemBlock("foo")("bar", { baz: true }) returns "foo__bar foo__bar--baz"', t => {
+test('calling bemJoin("foo")("bar", { baz: true }) returns "foo__bar foo__bar--baz"', t => {
 	t.is(
-		bemBlock('foo')('bar', { baz: true }),
+		bemJoin('foo')('bar', { baz: true }),
 		'foo__bar foo__bar--baz',
 	)
 })
 
-test('calling bemBlock("foo")("bar", { baz: false }) returns "foo__bar"', t => {
+test('calling bemJoin("foo")("bar", { baz: false }) returns "foo__bar"', t => {
 	t.is(
-		bemBlock('foo')('bar', { baz: false }),
+		bemJoin('foo')('bar', { baz: false }),
 		'foo__bar',
 	)
 })
 
-test('calling bemBlock("foo")("bar", { baz: true, qux: false }) returns "foo__bar foo__bar--baz"', t => {
+test('calling bemJoin("foo")("bar", { baz: true, qux: false }) returns "foo__bar foo__bar--baz"', t => {
 	t.is(
-		bemBlock('foo')('bar', { baz: true, qux: false }),
+		bemJoin('foo')('bar', { baz: true, qux: false }),
 		'foo__bar foo__bar--baz',
 	)
 })
 
-test('calling bemBlock with nothing curries the default options', t => {
+test('calling bemJoin with nothing curries the default options', t => {
 	t.is(
-		bemBlock()('foo')('bar', { baz: true }),
+		bemJoin()('foo')('bar', { baz: true }),
 		'foo__bar foo__bar--baz',
 	)
 })
 
-test('calling bemBlock with options curries the options', t => {
+test('calling bemJoin with options curries the options', t => {
 	t.is(
-		bemBlock({
+		bemJoin({
 			elementSeparator: 'xx',
 			modifierSeparator: 'yy',
 		})('foo')('bar', { baz: true }),
@@ -72,13 +72,13 @@ test('calling bemBlock with options curries the options', t => {
 	)
 })
 
-test('calling bemBlock with partial options uses default options for the missing ones', t => {
+test('calling bemJoin with partial options uses default options for the missing ones', t => {
 	t.is(
-		bemBlock({ elementSeparator: 'xx' })('foo')('bar', { baz: true }),
+		bemJoin({ elementSeparator: 'xx' })('foo')('bar', { baz: true }),
 		'fooxxbar fooxxbar--baz',
 	)
 	t.is(
-		bemBlock({ modifierSeparator: 'yy' })('foo')('bar', { baz: true }),
+		bemJoin({ modifierSeparator: 'yy' })('foo')('bar', { baz: true }),
 		'foo__bar foo__baryybaz',
 	)
 })
